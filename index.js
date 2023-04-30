42660ac3bdbad7b561bcb9c4c5808a3fc7ae7db8
Karina <karina.kvych@gmail.com>
2023-05-01 00:10:44 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// Feature #1
+let currentTime = document.querySelector("#time-string");
+let now = new Date();
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+currentTime.innerHTML = `${day} ${hours}:${minutes}`;
+
+// Feature#2
+function searchForCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#type-city");
+
+  let cityName = document.querySelector("h1");
+  if (searchInput.value) {
+    cityName.innerHTML = `${searchInput.value}`;
+  }
+}
+
+let cityForm = document.querySelector("#search-form");
+cityForm.addEventListener("submit", searchForCity);
+
+// Feature#3
+let celsiusType = document.querySelector("#celsius");
+let farengateType = document.querySelector("#farengate");
+function handlTemperature(temp, type) {
+  let temperature = document.querySelector("#number");
+  if (type === "celsius") {
+    temperature.innerHTML = `8`;
+    celsiusType.className = "active-temp";
+    farengateType.className = "";
+  } else {
+    temperature.innerHTML = `55`;
+    farengateType.className = "active-temp";
+    celsiusType.className = "";
+  }
+}
+
+celsiusType.addEventListener("click",function(){
+handlTemperature(8,"celsius");
+});
+
+farengateType.addEventListener("click",function(){
+  handlTemperature(55,"farengate");
+});
